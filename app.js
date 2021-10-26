e8521bd1532488d50e032fbd14eb01125a892624
tranvuluan <tranvuluan069@gmail.com>
2021-10-26 08:31:58 +0700

    menu

--- app.js
+++ app.js
@@ -3,4 +3,14 @@ $(document).ready(() => {
         $('#hamburger-menu').toggleClass('active');
         $('#nav-menu').toggleClass('active');
     })
+
+    $('#user-circle').click(() => {
+        $('#option-user').toggleClass('active');
+        $('#search-input').removeClass('active');
+    })
+
+    $('#search').click(() => {
+        $('#option-user').removeClass('active');
+        $('#search-input').toggleClass('active');
+    })
 });
